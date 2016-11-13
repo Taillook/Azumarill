a4b48361a3ad4294f76a57126e4e55c42b9d0386
Taillook <jousoku@gmail.com>
2016-11-13 15:54:30 +0900

    行列クラスの作成

--- opMatrix.py
+++ opMatrix.py
@@ -1,3 +1,17 @@
+class OpMatrix:
+	def __init__(self):
+		self.mtr=[]
+
+	def append(self,lst):
+		self.mtr.append(lst)
+
+	def __str__(self):
+		smtr = ""
+		for i in xrange(len(self.mtr)):
+			smtr+=("|"+" ".join(map(str,self.mtr[i]))+"|")
+			if i!=len(self.mtr)-1: smtr+="\n"
+		return smtr
+
 def finDet2(a,b,c,d):
 	print "|"+str(a)+" "+str(b)+"|"
 	print "|"+str(c)+" "+str(d)+"|\n"
@@ -9,4 +23,8 @@ def findDet3(a,b,c,d,e,f,g,h,i):
 	print "|"+str(g)+" "+str(h)+" "+str(i)+"|\n"
 
 if __name__ == '__main__':
+	mtr = OpMatrix()
+	mtr.append(map(int,raw_input().split()))
+	mtr.append(map(int,raw_input().split()))
+	print mtr
 	finDet2(1,2,3,4)
